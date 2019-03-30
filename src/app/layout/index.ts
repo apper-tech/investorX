e5031187699643d9fa5829f325715c1825849585
mh-mazen <mcs7dp3@gmail.com>
2019-03-30 15:19:34 +0300

    Auto-generated commit

--- src/app/layout/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './app-footer/app-footer.component';
-export * from './app-header/app-header.component';
-export * from './app-layout/app-layout.component';
-export * from './app-nav/app-nav.component';
